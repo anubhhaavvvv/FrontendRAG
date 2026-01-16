9a76cad3bfabf0f2c958b86a6070d2a83de83ed6
[dyad] <git@dyad.sh>
2026-01-16 11:27:29 +0530

    [dyad] wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-950 p-4">
       <Chatbot />
       <MadeWithDyad />
     </div>
